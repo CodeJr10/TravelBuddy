f2c59a9482e79822d4475e6dc31da5bb944a737f
CodeJr10 <aadityamalap666@gmail.com>
2025-04-29 23:08:02 +0530

    Added Navbar component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,22 +3,29 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="">
-        <div className="">
-          <div className="flex justify-between items-center bg-gray-800 p-4 text-white">
-            <div className="text-lg font-bold">Travel Buddy</div>
-            <div className="space-x-4">
-              <Link to="/" className="hover:text-gray-400">
-                Home
-              </Link>
-              <Link to="/about" className="hover:text-gray-400">
-                About
-              </Link>
-              <Link to="/contact" className="hover:text-gray-400">
-                Contact
-              </Link>
-            </div>
+    <div className="">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between items-center py-4 text-black">
+          <div className="text-2xl font-bold">Travel Buddy</div>
+          <div className="space-x-6 flex items-center">
+            <Link to="/about" className="hover:text-blue-600 font-medium">
+              About
+            </Link>
+            <Link to="/contact" className="hover:text-blue-600 font-medium">
+              Contact
+            </Link>
+            <Link
+              to="/login"
+              className="px-4 py-2  text-blue-500 rounded-full hover:bg-blue-50 transition"
+            >
+              Login
+            </Link>
+            <Link
+              to="/signup"
+              className="px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition"
+            >
+              Sign Up
+            </Link>
           </div>
         </div>
       </div>
